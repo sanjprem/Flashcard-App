8e73f241642a853930ad9172d53058155c525c3a
sanjprem <sanj.prem@outlook.com>
2022-08-26 12:49:21 -0400

    reformatted code and added todo items

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 const cards = [
     {
-      front: 'The "First Computer Programmer"',
-      back: 'Ada Lovelace',
-      flipped: false,
+        front: 'The "First Computer Programmer"',
+        back: 'Ada Lovelace',
+        flipped: false,
     },
     {
-      front: 'Invented the "Clarke Calculator"',
-      back: 'Edith Clarke',
-      flipped: false,
-  
-    },
-    {
-      front: 'Famous World War II Enigma code breaker',
-      back: 'Alan Turing',
-      flipped: false,
-    },
-    {
-      front: 'Created satellite orbit analyzation software for NASA',
-      back: 'Dr. Evelyn Boyd Granville',
-      flipped: false,
-    },
-  ]; 
+        front: 'Invented the "Clarke Calculator"',
+        back: 'Edith Clarke',
+        flipped: false,
 
-  new Vue({
+    },
+    {
+        front: 'Famous World War II Enigma code breaker',
+        back: 'Alan Turing',
+        flipped: false,
+    },
+    {
+        front: 'Created satellite orbit analyzation software for NASA',
+        back: 'Dr. Evelyn Boyd Granville',
+        flipped: false,
+    },
+];
+
+// display our data
+// on click: flip cards back and forth
+// get info for new cards from the user
+// add new card when user hits enter or clicks button
+// delete cards
+// animate card flip
+// display an error message if form fields are blank
+new Vue({
     el: '#flashcard-app',
     data: {
-      cards: cards,
+        cards: cards,
     },
-    methods: {
-
-    }
-  });
+    methods: {}
+});
